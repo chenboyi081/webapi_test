1fa52e58f38fe1984a6c32197b3a175a988edc84
cboii <chenboyi081@163.com>
2015-10-08 08:51:39 +0800

    first commit

---
+++ 算法测试 2015-09-25/Program.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 算法测试_2015_09_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region MSDN 下面的代码示例创建一个 TripleDESCryptoServiceProvider 对象并使用该对象加密和解密文件中的数据。
+            try
+            {
+                // Create a new TripleDESCryptoServiceProvider object
+                // to generate a key and initialization vector (IV).
+                TripleDESCryptoServiceProvider tDESalg = new TripleDESCryptoServiceProvider();
+
+                // Create a string to encrypt.
+                string sData = "Here is some data to encrypt.";
+                string FileName = "CText.txt";
+
+                // Encrypt text to a file using the file name, key, and IV.
+                EncryptTextToFile(sData, FileName, tDESalg.Key, tDESalg.IV);
+
+                // Decrypt the text from a file using the file name, key, and IV.
+                string Final = DecryptTextFromFile(FileName, tDESalg.Key, tDESalg.IV);
+
+                // Display the decrypted string to the console.
+                Console.WriteLine(Final);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            } 
+            #endregion
+
+            #region MSDN 下面的代码示例创建一个 TripleDESCryptoServiceProvider 对象并使用该对象加密和解密内存中的数据。
+            try
+            {
+                // Create a new TripleDESCryptoServiceProvider object
+                // to generate a key and initialization vector (IV).
+                TripleDESCryptoServiceProvider tDESalg = new TripleDESCryptoServiceProvider();
+
+                // Create a string to encrypt.
+                string sData = "Here is some data to encrypt.";
+
+                // Encrypt the string to an in-memory buffer.
+                byte[] Data = EncryptTextToMemory(sData, tDESalg.Key, tDESalg.IV);
+
+                // Decrypt the buffer back to a string.
+                string Final = DecryptTextFromMemory(Data, tDESalg.Key, tDESalg.IV);
+
+                // Display the decrypted string to the console.
+                Console.WriteLine(Final);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            } 
+            #endregion
+
+            //string tx = btnI2Gen("sidfaionfoa");
+            #region link 3DES加解密 
+            Console.WriteLine("Encrypt String...");
+            txtKey = "tkGGRmBErvc=";      
+            btnKeyGen();
+            Console.WriteLine("Encrypt Key :{0}", txtKey);
+            txtIV = "Kl7ZgtM1dvQ=";
+            btnIVGen();
+            Console.WriteLine("Encrypt IV :{0}", txtIV);
+            Console.WriteLine();
+            string txtEncrypted = EncryptString("1111");
+            Console.WriteLine("Encrypt String : {0}", txtEncrypted);
+            string txtOriginal = DecryptString(txtEncrypted);
+            Console.WriteLine("Decrypt String : {0}", txtOriginal); 
+            #endregion
+
+            Console.ReadLine();
+        }
+        
+
+         public static void EncryptTextToFile(String Data, String FileName, byte[] Key, byte[] IV)
+         {
+             try
+             {
+                 // Create or open the specified file.
+                 FileStream fStream = File.Open(FileName,FileMode.OpenOrCreate);
+
+                 // Create a CryptoStream using the FileStream 
+                 // and the passed key and initialization vector (IV).
+                 CryptoStream cStream = new CryptoStream(fStream, 
+                     new TripleDESCryptoServiceProvider().CreateEncryptor(Key,IV), 
+                     CryptoStreamMode.Write); 
+
+                 // Create a StreamWriter using the CryptoStream.
+                 StreamWriter sWriter = new StreamWriter(cStream);
+
+                 // Write the data to the stream 
+                 // to encrypt it.
+                 sWriter.WriteLine(Data);
+
+                 // Close the streams and
+                 // close the file.
+                 sWriter.Close();
+                 cStream.Close();
+                 fStream.Close();
+             }
+             catch(CryptographicException e)
+             {
+                 Console.WriteLine("A Cryptographic error occurred: {0}", e.Message);
+             }
+             catch(UnauthorizedAccessException  e)
+             {
+                 Console.WriteLine("A file access error occurred: {0}", e.Message);
+             }
+
+         }
+
+         public static string DecryptTextFromFile(String FileName, byte[] Key, byte[] IV)
+         {
+             try
+             {
+                 // Create or open the specified file. 
+                 FileStream fStream = File.Open(FileName, FileMode.OpenOrCreate);
+
+                 // Create a CryptoStream using the FileStream 
+                 // and the passed key and initialization vector (IV).
+                 CryptoStream cStream = new CryptoStream(fStream, 
+                     new TripleDESCryptoServiceProvider().CreateDecryptor(Key,IV), 
+                     CryptoStreamMode.Read); 
+
+                 // Create a StreamReader using the CryptoStream.
+                 StreamReader sReader = new StreamReader(cStream);
+
+                 // Read the data from the stream 
+                 // to decrypt it.
+                 string val = sReader.ReadLine();
+
+                 // Close the streams and
+                 // close the file.
+                 sReader.Close();
+                 cStream.Close();
+                 fStream.Close();
+
+                 // Return the string. 
+                 return val;
+             }
+             catch(CryptographicException e)
+             {
+                 Console.WriteLine("A Cryptographic error occurred: {0}", e.Message);
+                 return null;
+             }
+             catch(UnauthorizedAccessException  e)
+             {
+                 Console.WriteLine("A file access error occurred: {0}", e.Message);
+                 return null;
+             }
+         }
+
+         public static byte[] EncryptTextToMemory(string Data, byte[] Key, byte[] IV)
+         {
+             try
+             {
+                 // Create a MemoryStream.
+                 MemoryStream mStream = new MemoryStream();
+
+                 // Create a CryptoStream using the MemoryStream 
+                 // and the passed key and initialization vector (IV).
+                 CryptoStream cStream = new CryptoStream(mStream,
+                     new TripleDESCryptoServiceProvider().CreateEncryptor(Key, IV),
+                     CryptoStreamMode.Write);
+
+                 // Convert the passed string to a byte array.
+                 byte[] toEncrypt = new ASCIIEncoding().GetBytes(Data);
+
+                 // Write the byte array to the crypto stream and flush it.
+                 cStream.Write(toEncrypt, 0, toEncrypt.Length);
+                 cStream.FlushFinalBlock();
+
+                 // Get an array of bytes from the 
+                 // MemoryStream that holds the 
+                 // encrypted data.
+                 byte[] ret = mStream.ToArray();
+
+                 // Close the streams.
+                 cStream.Close();
+                 mStream.Close();
+
+                 // Return the encrypted buffer.
+                 return ret;
+             }
+             catch (CryptographicException e)
+             {
+                 Console.WriteLine("A Cryptographic error occurred: {0}", e.Message);
+                 return null;
+             }
+
+         }
+
+         public static string DecryptTextFromMemory(byte[] Data, byte[] Key, byte[] IV)
+         {
+             try
+             {
+                 // Create a new MemoryStream using the passed 
+                 // array of encrypted data.
+                 MemoryStream msDecrypt = new MemoryStream(Data);
+
+                 // Create a CryptoStream using the MemoryStream 
+                 // and the passed key and initialization vector (IV).
+                 CryptoStream csDecrypt = new CryptoStream(msDecrypt,
+                     new TripleDESCryptoServiceProvider().CreateDecryptor(Key, IV),
+                     CryptoStreamMode.Read);
+
+                 // Create buffer to hold the decrypted data.
+                 byte[] fromEncrypt = new byte[Data.Length];
+
+                 // Read the decrypted data out of the crypto stream
+                 // and place it into the temporary buffer.
+                 csDecrypt.Read(fromEncrypt, 0, fromEncrypt.Length);
+
+                 //Convert the buffer into a string and return it.
+                 return new ASCIIEncoding().GetString(fromEncrypt);
+             }
+             catch (CryptographicException e)
+             {
+                 Console.WriteLine("A Cryptographic error occurred: {0}", e.Message);
+                 return null;
+             }
+         }
+
+         #region 3DES加密 + link：http://www.cnblogs.com/hcbin/archive/2010/08/30/1812353.html
+         private static SymmetricAlgorithm mCSP;
+         private static string txtKey;       //密钥，必须12位
+         private static string txtIV;        //向量，必须是12个字符
+         private static void btnKeyGen()
+         {
+             mCSP = SetEnc();
+             byte[] byt2 = Convert.FromBase64String(txtKey);
+             mCSP.Key = byt2;
+         }
+         private static void btnIVGen()
+         {
+             byte[] byt2 = Convert.FromBase64String(txtIV);
+             mCSP.IV = byt2;
+         }
+         //private static string btnI2Gen(string txt)
+         //{
+         //    byte[] byteArray =System.Text.Encoding.Default.GetBytes(txt);
+         //    return Convert.ToBase64String(byteArray);
+         //}
+         private static string EncryptString(string Value)
+         {
+             ICryptoTransform ct;
+             MemoryStream ms;
+             CryptoStream cs;
+             byte[] byt;
+             ct = mCSP.CreateEncryptor(mCSP.Key, mCSP.IV);
+             byt = Encoding.UTF8.GetBytes(Value);
+             ms = new MemoryStream();
+             cs = new CryptoStream(ms, ct, CryptoStreamMode.Write);
+             cs.Write(byt, 0, byt.Length);
+             cs.FlushFinalBlock();
+             cs.Close();
+             return Convert.ToBase64String(ms.ToArray());
+         }
+         private static string DecryptString(string Value)
+         {
+             ICryptoTransform ct;
+             MemoryStream ms;
+             CryptoStream cs;
+             byte[] byt;
+             ct = mCSP.CreateDecryptor(mCSP.Key, mCSP.IV);
+             byt = Convert.FromBase64String(Value);
+             ms = new MemoryStream();
+             cs = new CryptoStream(ms, ct, CryptoStreamMode.Write);
+             cs.Write(byt, 0, byt.Length);
+             cs.FlushFinalBlock();
+             cs.Close();
+             return Encoding.UTF8.GetString(ms.ToArray());
+         }
+         private static SymmetricAlgorithm SetEnc()
+         {
+             return new DESCryptoServiceProvider();
+         }
+         #endregion
+    }
+}
---
+++ 算法测试2015-09-24/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 算法测试2015_09_24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Security s = new Security();
+            //s.InitialReg("/rsaPath");           // 初始化注册表，程序运行时调用，在调用之前更新公钥xml
+            s.RSAKey("../PrivateKey.xml", "../PublicKey.xml");    //生成rsa公私钥  C:\Users\Administrator\Desktop\webapi相关测试\算法测试2015-09-24\bin 下可以找到
+            string PrivateKey = s.ReadPrivateKey("../PrivateKey.xml");
+            string PublicKey = s.ReadPrivateKey("../PublicKey.xml");
+          
+          
+            string HDInfo = s.GetHardID();      //获取用户硬盘号
+            string HDInfoRsaStr = s.RSAEncrypt(PublicKey, HDInfo);   //服务端对用户硬盘号进行RSA加密
+            string HDInfoDRsaStr = s.RSADecrypt(PrivateKey, HDInfoRsaStr);  //客户端用公钥解密
+            string HDInfoMD5Str = s.GetHash(HDInfoDRsaStr);      //客户端对解密后的数据用MD5加密
+
+            //string strContent = "我是地球人，我是中国人，我是浙江人，我是温州人！";     //发送消息
+            //string strContent = "Hello!";
+            //string Md5Str = s.GetHash(strContent);      //对字符进行MD5加密
+            //string RsaStr = s.RSAEncrypt(PublicKey, Md5Str);       // RSA加密
+            //string DRsaStr = s.RSADecrypt(PrivateKey,RsaStr);      // RSA解密
+
+
+            #region 用户签名验证
+            string usernameAndPwd = "cby" + "123456";
+            //string code = "123456";         //注册码
+            string usernameAndPwdMd5Str = s.GetHash(usernameAndPwd);      //客户端对用户名进行MD5加密
+            string codeMd5Str = s.GetHash(usernameAndPwd);      //对验证码进行MD5加密
+            string SignatureStr = s.SignatureFormatter(PrivateKey, usernameAndPwdMd5Str);      //对MD5加密后的密文进行签名
+            bool isRihtSignature = s.SignatureDeformatter(PublicKey, usernameAndPwdMd5Str, SignatureStr);      //签名验证 
+            #endregion
+
+
+
+            #region 使用System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile方法进行MD5和SHA1的加密
+            //string temStr1 = "用我来计算SHA1值吧!";
+            //string temStr2 = "用你来计算SHA1值吧!";
+           
+            //string SHA1Str1 = s.EncryptByHashPasswordForStoringInConfigFileMethod(temStr1, "SHA1");
+            //string SHA1Str2 = s.EncryptByHashPasswordForStoringInConfigFileMethod(temStr2, "SHA1");
+            //Console.WriteLine("SHA1计算所得值1：" + SHA1Str1 + "\r\n" + "SHA1计算所得值2：" + SHA1Str2);
+
+            //string MD5Str1 = s.EncryptByHashPasswordForStoringInConfigFileMethod(temStr1, "MD5");
+            //string MD5Str2 = s.EncryptByHashPasswordForStoringInConfigFileMethod(temStr2, "MD5");
+            //Console.WriteLine("MD5计算所得值1：" + MD5Str1 + "\r\n" + "MD5计算所得值2：" + MD5Str2); 
+            #endregion
+
+            //Console.ReadKey();
+        }
+
+    }
+}
